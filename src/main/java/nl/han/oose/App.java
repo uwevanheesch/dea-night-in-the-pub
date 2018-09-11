60a0f75d26349d4220330478cf44b7064e619f0d
Uwe van Heesch <uwe@vanheesch.net>
2018-09-11 13:24:37 +0200

    Initial commit

---
+++ src/main/java/nl/han/oose/App.java
@@ -0,0 +1,13 @@
+package nl.han.oose;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
